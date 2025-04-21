179824b0398022e754fd38bc3284378444b70715
Ironkeeper <jldeciga@hotmail.com>
2025-04-21 13:00:31 -0600

    segundo commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,5 +15,5 @@
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
-    <p>Titit que tal</p>
+    <p>Titit que tal2</p>
 </x-layouts.app>
