a83dc011f5fe9f2dded72863d8b772513782690a
Ironkeeper <jldeciga@hotmail.com>
2025-04-21 12:39:26 -0600

    Mi primer Comit

---
+++ bootstrap/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+        then: function(){
+            //con esto le decimos a laravel que hemos creado un nuevo archivo de rutas
+            //ademas le digo que solo ingresen los que esten autenticados con auth
+            Route::middleware('web','auth')
+            ->prefix('admin')
+            ->name('admin.')
+            ->group(base_path('routes/admin.php'));
+        }
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
